ff6fd230fad98829cf9d494cd020c7e496c36bdb
Sathish-Ramasundaram <sathishramasundaram@gmail.com>
2025-10-31 13:00:21 +0530

    Update vite.config.js

--- 007SPA/vite.config.js
+++ 007SPA/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Project/007SPA/', // matches your GitHub repo and subfolder
+  base: '/Project/007SPA/' // matches your GitHub repo and subfolder
 });
